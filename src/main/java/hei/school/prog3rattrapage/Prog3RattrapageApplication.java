c40717ea05a68d15552b873acb21b5539e12763e
nekena86 <hei.nekena.6@gmail.com>
2026-09-15 08:15:23 +0300

    first commit

---
+++ src/main/java/hei/school/prog3rattrapage/Prog3RattrapageApplication.java
@@ -0,0 +1,13 @@
+package hei.school.prog3rattrapage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3RattrapageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog3RattrapageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/school/prog3rattrapage/Prog3RattrapageApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.prog3rattrapage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3RattrapageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
